c5e8abc6e29ca37f6bff3c814c20766c77fe7983
WydD <loic.petit@gmail.com>
2017-06-09 02:40:07 +0200

    Working version using easyhook

---
+++ display_input/Injector.cpp
@@ -0,0 +1,61 @@
+// Injector.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+#include <easyhook.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Loading input display injection!\n";
+
+	HWND winhandle = FindWindow(NULL, L"The King of Fighters XIV");
+	if (!winhandle) {
+		std::cout << "KOF XIV is not running!\n";
+
+
+		std::wcout << "Press Enter to exit\n";
+		std::wstring input;
+		std::getline(std::wcin, input);
+		std::getline(std::wcin, input);
+		return 1;
+	}
+	DWORD processId;
+	GetWindowThreadProcessId(winhandle, &processId);
+
+	WCHAR* dllToInject = L"BeepHook.dll";
+	wprintf(L"Attempting to inject: %s\n\n", dllToInject);
+
+	// Inject dllToInject into the target process Id, passing 
+	// freqOffset as the pass through data.
+	NTSTATUS nt = RhInjectLibrary(
+		processId,   // The process to inject into
+		0,           // ThreadId to wake up upon injection
+		EASYHOOK_INJECT_DEFAULT,
+		NULL, // 32-bit
+		dllToInject,		 // 64-bit not provided
+		NULL,
+		0
+	);
+
+	if (nt != 0)
+	{
+		printf("RhInjectLibrary failed with error code = %d\n", nt);
+		PWCHAR err = RtlGetLastErrorString();
+		std::wcout << err << "\n";
+	}
+	else
+	{
+		std::wcout << L"Library injected successfully.\n";
+	}
+
+	std::wcout << "Press Enter to exit";
+	std::wstring input;
+	std::getline(std::wcin, input);
+	std::getline(std::wcin, input);
+	return 0;
+}
---
+++ display_input_hook/input_display_hook.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+
+#include <easyhook.h>
+#include <string>
+#include <iostream>
+#include <Windows.h>
+#include <Psapi.h>
+
+void(*DrawKeyHistory)(void *obj, int playerID, void* history, bool bFrameDraw) = NULL;
+void* funcPtr = NULL;
+void HookedDrawKeyHistory(void *obj, int playerID, void* history, bool bFrameDraw)
+{
+	return DrawKeyHistory(obj, playerID, history, true);
+}
+
+// EasyHook will be looking for this export to support DLL injection. If not found then 
+// DLL injection will fail.
+extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo);
+
+void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
+{
+	BYTE lookup[] = { 0x48, 0x89, 0x5C, 0x24, 0x08, 0x48, 0x89, 0x6C, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x18, 0x57, 0x48, 0x83, 0xEC, 0x30, 0x48, 0x8B, 0x41, 0x08, 0x48, 0x8B, 0xD9, 0x48, 0x83, 0xC1, 0x08, 0x41, 0x0F, 0xB6, 0xF9, 0x49, 0x8B, 0xE8, 0x8B, 0xF2, 0xFF, 0x10, 0x84, 0xC0, 0x75, 0x3D, 0x38, 0x43 };
+
+	MODULEINFO moduleInfo;
+	GetModuleInformation(GetCurrentProcess(), GetModuleHandle(TEXT("kofxiv.exe")), &moduleInfo, sizeof(moduleInfo));
+
+	BYTE* end = (BYTE*)(moduleInfo.lpBaseOfDll) + moduleInfo.SizeOfImage;
+	for (BYTE* current = (BYTE*)moduleInfo.lpBaseOfDll; current < end; ++current) {
+		if (*current != lookup[0]) {
+			continue;
+		}
+		BOOL found = true;
+		for (int i = 1; i < sizeof(lookup); ++i) {
+			if (current[i] != lookup[i]) {
+				found = false;
+				break;
+			}
+		}
+		if (found) {
+			DrawKeyHistory = (void(*)(void *, int, void*, bool))current;
+			break;
+		}
+	}
+
+	if (!DrawKeyHistory) {
+		return;
+	}
+
+	// Perform hooking
+	HOOK_TRACE_INFO hHook = { NULL }; // keep track of our hook
+
+									  // Install the hook
+	NTSTATUS result = LhInstallHook(
+		DrawKeyHistory,
+		HookedDrawKeyHistory,
+		NULL,
+		&hHook);
+
+	// If the threadId in the ACL is set to 0,
+	// then internally EasyHook uses GetCurrentThreadId()
+	ULONG ACLEntries[1] = { 0 };
+
+	// Disable the hook for the provided threadIds, enable for all others
+	LhSetExclusiveACL(ACLEntries, 1, &hHook);
+
+	return;
+}
